67262c539be3a68f2672caa646110d15cc2a2124
Chaw Su Thaw Tar <chawsuthawtar@my.jcu.edu.au>
2018-11-20 14:04:47 +0800

    add name printing

--- oddname.py
+++ oddname.py
@@ -1,1 +1,12 @@
 """Input a name and print alternate characters"""
+def main():
+    name = input("Enter the name")
+
+    while len(name)<= 1:
+        print ("Enter name")
+        name = input("name")
+    print (name[::2])
+    for i in range(0,len(name),2):
+        print(name(i))
+
+main()
